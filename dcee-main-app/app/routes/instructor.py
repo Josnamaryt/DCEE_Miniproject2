ccfe486397e0960c121dbc74f8dddf1c4d3311ed
Josnamaryt <josnamarythomas25@gmail.com>
2025-01-15 16:30:17 +0530

    Figma, DB, UML
    New modules

--- dcee-main-app/app/routes/instructor.py
+++ dcee-main-app/app/routes/instructor.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from flask_login import login_user, logout_user, login_required 
+from flask_login import login_user, logout_user, login_required, current_user
 # from app import login_manager
 
 instructor_bp = Blueprint('instructor', __name__)
@@ -7,4 +7,5 @@ instructor_bp = Blueprint('instructor', __name__)
 @instructor_bp.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('instructor/dashboard.html')
+    user_name = f"{current_user.first_name} {current_user.last_name}"
+    return render_template('instructor/dashboard.html', user_name=user_name)
